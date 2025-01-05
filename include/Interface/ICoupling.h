6063507a40570a1a146bf8971fdec30c8092eb09
Rmain Kord <raminkord92@gmail.com>
2025-01-05 23:09:30 +0330

    Unused method in crtp base class ICoupling removed

--- include/Interface/ICoupling.h
+++ include/Interface/ICoupling.h
@@ -8,10 +8,6 @@ namespace PDFxTMD
 template <typename Derived> class ICoupling : public CRTPBase<Derived>
 {
   public:
-    void initialize(const std::vector<double> &mu2_vec, const std::vector<double> &alphasVec)
-    {
-        return this->derived().initialize(mu2_vec, alphasVec);
-    }
     double AlphaQCDMu2(double mu2)
     {
         return this->derived().AlphaQCD(mu2);
