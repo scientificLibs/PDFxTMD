e2688883826dd70ed06e3258bd27d220bd5ef7a4
Ramin Kord Valeshabadi <raminkord92@gmail.com>
2025-05-16 20:43:38 +0330

    update numparser for macos support

--- include/PDFxTMDLib/Common/NumParser.h
+++ include/PDFxTMDLib/Common/NumParser.h
@@ -1,7 +1,9 @@
 #pragma once
-#include <charconv>
 #include <cstring>
 #include <string_view>
+#include <cstdlib>
+#include <cerrno>
+#include <type_traits>
 
 namespace PDFxTMD
 {
@@ -17,14 +19,14 @@ class NumParser
     void reset(std::string_view input) noexcept
     {
         _current = input.data();
-        _end = input.data() + input.size();
+        _end     = input.data() + input.size();
     }
 
-    template <typename T> bool operator>>(T &value) noexcept
+    template <typename T>
+    bool operator>>(T &value) noexcept
     {
         skipSpaces();
-        auto result = parseNumber<T>(value);
-        return result;
+        return parseNumber(value);
     }
 
     [[nodiscard]] bool hasMore() const noexcept
@@ -39,19 +41,40 @@ class NumParser
     void skipSpaces() noexcept
     {
         while (_current < _end && *_current == ' ')
-        {
             ++_current;
-        }
     }
 
-    template <typename T> bool parseNumber(T &value) noexcept
+    template <typename T>
+    bool parseNumber(T &value) noexcept
     {
-        auto [ptr, ec] = std::from_chars(_current, _end, value);
-        if (ec == std::errc::invalid_argument || ec == std::errc::result_out_of_range)
-        {
-            return false;
+        char *next = nullptr;
+        errno = 0;
+
+        if constexpr (std::is_integral_v<T>) {
+            // parse as signed long long, then cast
+            long long tmp = std::strtoll(_current, &next, /*base=*/10);
+            if (_current == next || errno == ERANGE)
+                return false;
+            value = static_cast<T>(tmp);
         }
-        _current = ptr;
+        else if constexpr (std::is_unsigned_v<T>) {
+            unsigned long long tmp = std::strtoull(_current, &next, /*base=*/10);
+            if (_current == next || errno == ERANGE)
+                return false;
+            value = static_cast<T>(tmp);
+        }
+        else if constexpr (std::is_floating_point_v<T>) {
+            long double tmp = std::strtold(_current, &next);
+            if (_current == next || errno == ERANGE)
+                return false;
+            value = static_cast<T>(tmp);
+        }
+        else {
+            static_assert(!sizeof(T*), "parseNumber only supports integral or floating types");
+        }
+
+        // advance past what we just consumed:
+        _current = next;
         return true;
     }
 };
