4ad96fec435f1c735e1d4c0a230985d3588878fc
Ramin Kord Valeshabadi <raminkord92@gmail.com>
2025-05-23 16:59:26 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
     platforms=["any"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: GNU General Public License v3.0",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
